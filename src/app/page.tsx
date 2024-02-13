40546862e004eecfa7367baec0d51a6ff28025e8
Peter <bklynpeter@gmail.com>
2024-02-13 18:20:01 -0500

    integrate Docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-    return (
-        <main className="">
-           Hello World, nice to meet you!
-        </main>
-    )
+    return <main>Hello World, nice to meet you!</main>
 }
