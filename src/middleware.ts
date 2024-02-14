d48ddbdef6ac97a8ff1a81d5dc039c42dfe6ed48
Peter <bklynpeter@gmail.com>
2024-02-14 17:58:13 -0500

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server'
-const isLoggedIn = true
+const isLoggedIn = false
 
 export function middleware(request: NextRequest) {
     if (!isLoggedIn) return NextResponse.redirect(new URL('/', request.url))
